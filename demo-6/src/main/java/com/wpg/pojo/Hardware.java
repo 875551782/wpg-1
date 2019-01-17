fe5ef3716cc4a088ad5428ba015b8a10d12259d9
chenth <15771965389@163.com>
2019-01-17 15:42:01 +0800

    chenth:create hardware

--- demo-6/src/main/java/com/wpg/pojo/Hardware.java
+++ demo-6/src/main/java/com/wpg/pojo/Hardware.java
@@ -2,4 +2,69 @@ package com.wpg.pojo;
 
 public class Hardware {
 
+	private Integer id;
+	private String module;
+	private String name;
+	private String type;
+	private String unit;
+	private String brand;
+	private Integer mark;
+	private Integer dFalg;
+	private Double price;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getModule() {
+		return module;
+	}
+	public void setModule(String module) {
+		this.module = module;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getUnit() {
+		return unit;
+	}
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public Integer getMark() {
+		return mark;
+	}
+	public void setMark(Integer mark) {
+		this.mark = mark;
+	}
+	public Integer getdFalg() {
+		return dFalg;
+	}
+	public void setdFalg(Integer dFalg) {
+		this.dFalg = dFalg;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	
 }
