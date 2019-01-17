63c53fd0f0f400045cf86dcf60823fb63f68e63a
sunxf <sunxf@SH-RAD02-V57.shwpg.com>
2019-01-17 16:58:13 +0800

    sunxf:hardwareService

--- demo-6/src/main/java/com/wpg/service/HardwareService.java
+++ demo-6/src/main/java/com/wpg/service/HardwareService.java
@@ -14,19 +14,29 @@ public class HardwareService {
 	@Autowired
 	private HardwareDao hardwareDao;
 	
+	//查询所有物料
+	public List<Hardware> selAllHardware(){
+		return hardwareDao.selAllHardware();
+	}
+	
+	//按功能查询物料
 	public List<Hardware> selHardwareByModule(String module){
 		return hardwareDao.selHardwareByModule(module);
 	}
 	
+	//增加物料类
 	public void addHardware_Group(Hardware_Group group) {
 		hardwareDao.addHardware_Group(group);
 	}
 	
+	//增加新物料
 	public void addHardWare(Hardware hardware) {
 		hardwareDao.addHardWare(hardware);
 	}
 	
+	//更新物料信息
 	public void updateHardWare(Hardware hardware) {
 		hardwareDao.updateHardWare(hardware);
 	}
+
 }
