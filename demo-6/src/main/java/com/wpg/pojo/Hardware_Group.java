d5b49a87ccb5c97f10c7d1560a77f0838cd5ffa1
chenth <15771965389@163.com>
2019-01-17 16:04:43 +0800

    chenth:change pojo

--- demo-6/src/main/java/com/wpg/pojo/Hardware_Group.java
+++ demo-6/src/main/java/com/wpg/pojo/Hardware_Group.java
@@ -2,4 +2,30 @@ package com.wpg.pojo;
 
 public class Hardware_Group {
 
+	private Integer mark;
+	private String desct;
+	private Integer states;
+	
+	
+	public Integer getMark() {
+		return mark;
+	}
+	public void setMark(Integer mark) {
+		this.mark = mark;
+	}
+	public String getDesct() {
+		return desct;
+	}
+	public void setDesct(String desct) {
+		this.desct = desct;
+	}
+	public Integer getStates() {
+		return states;
+	}
+	public void setStates(Integer states) {
+		this.states = states;
+	}
+	
+	
+	
 }
