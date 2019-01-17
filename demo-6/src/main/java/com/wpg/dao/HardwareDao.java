76f9b010b45df214bb0a384d9b5c0f76738f6741
sunxf <sunxf@SH-RAD02-V57.shwpg.com>
2019-01-17 16:25:14 +0800

    sunxf:back_controller,hardwaredao,hardwareservice,hardwareMapper

--- demo-6/src/main/java/com/wpg/dao/HardwareDao.java
+++ demo-6/src/main/java/com/wpg/dao/HardwareDao.java
@@ -3,8 +3,22 @@ package com.wpg.dao;
 import java.util.List;
 
 import com.wpg.pojo.Hardware;
+import com.wpg.pojo.Hardware_Group;
 
 public interface HardwareDao {
 
-	List<Hardware> selHardware();
+	//查询所有物料
+	public List<Hardware> selAllHardware();
+	
+	//按功能查询物料
+	public List<Hardware> selHardwareByModule(String module);
+	
+	//增加类
+	public void addHardware_Group(Hardware_Group group);
+	
+	//增加物料
+	public void addHardWare(Hardware hardware);
+	
+	//修改物料信息
+	public void updateHardWare(Hardware hardware);
 }
--- demo-6/src/main/java/com/wpg/service/HardwareService.java
+++ demo-6/src/main/java/com/wpg/service/HardwareService.java
@@ -1,13 +1,32 @@
 package com.wpg.service;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wpg.dao.HardwareDao;
+import com.wpg.pojo.Hardware;
+import com.wpg.pojo.Hardware_Group;
 
 @Service
 public class HardwareService {
 
 	@Autowired
 	private HardwareDao hardwareDao;
+	
+	public List<Hardware> selHardwareByModule(String module){
+		return hardwareDao.selHardwareByModule(module);
+	}
+	
+	public void addHardware_Group(Hardware_Group group) {
+		hardwareDao.addHardware_Group(group);
+	}
+	
+	public void addHardWare(Hardware hardware) {
+		hardwareDao.addHardWare(hardware);
+	}
+	
+	public void updateHardWare(Hardware hardware) {
+		hardwareDao.updateHardWare(hardware);
+	}
 }
