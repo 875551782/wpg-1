b581ea8ce32463c0779c3295bd7a29c6e6625934
chenth <15771965389@163.com>
2019-01-17 16:43:59 +0800

    chenth:change properties

--- demo-6/src/main/java/com/wpg/controller/Front_Controller.java
+++ demo-6/src/main/java/com/wpg/controller/Front_Controller.java
@@ -1,8 +1,25 @@
 package com.wpg.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wpg.pojo.Orders;
+import com.wpg.service.OrdersService;
 
 @Controller
 public class Front_Controller {
-
+	@Autowired
+	private OrdersService ordersService;
+	
+	@RequestMapping("user_showOrders.do")
+	@ResponseBody
+	public List<Orders> show(){
+		return ordersService.getAllOrders();
+	}
+	
+	
 }
--- demo-6/src/main/java/com/wpg/service/OrdersService.java
+++ demo-6/src/main/java/com/wpg/service/OrdersService.java
@@ -1,13 +1,20 @@
 package com.wpg.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wpg.dao.OrdersDao;
+import com.wpg.pojo.Orders;
 
 @Service
 public class OrdersService {
 
 	@Autowired
 	private OrdersDao ordersDao;
+	
+	public List<Orders> getAllOrders() {
+		return ordersDao.getAllOrders();		
+	}
 }
