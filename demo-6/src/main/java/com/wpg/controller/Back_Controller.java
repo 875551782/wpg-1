ec15f2b5ed778f0d69efdffe05a1bb32f81ace9a
sunxf <sunxf@SH-RAD02-V57.shwpg.com>
2019-01-17 16:11:53 +0800

    sunxf:Back_Controller

--- demo-6/src/main/java/com/wpg/controller/Back_Controller.java
+++ demo-6/src/main/java/com/wpg/controller/Back_Controller.java
@@ -1,8 +1,44 @@
 package com.wpg.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wpg.pojo.Hardware;
+import com.wpg.pojo.Hardware_Group;
+import com.wpg.service.HardwareService;
 
 @Controller
 public class Back_Controller {
-
+	
+	private HardwareService hardwareService;
+	
+	@RequestMapping("admin_viewHardware.do")
+	public String selHardware(ModelMap map, String module) {
+		List<Hardware> hardwares = hardwareService.selHardwareByModule(module);
+		map.addAttribute("hardwareList", hardwares);
+		return "";
+	}
+	
+	//增加物料数据
+	@RequestMapping("admin_insertHardware.do")
+	public String addHardware(ModelMap map, Hardware hardware) {
+		hardwareService.addHardWare(hardware);
+		return "";
+	}
+	
+	//增加一个新类
+	public String addHardware_Group(Hardware_Group group) {
+		hardwareService.addHardware_Group(group);
+		return "";
+	}
+	
+	//修改物料信息
+	public String updateHardWare(Hardware hardware) {
+		hardwareService.updateHardWare(hardware);
+		return "";
+	}
+	
 }
