37da2afdc1d4171d71ff9c7c8bc5af22f7748412
yanh <951003yanhan@163.com>
2019-01-18 10:22:45 +0800

    yanh

--- demo-6/src/main/java/com/wpg/controller/Back_Controller.java
+++ demo-6/src/main/java/com/wpg/controller/Back_Controller.java
@@ -15,7 +15,6 @@ public class Back_Controller {
 	
 	private HardwareService hardwareService;
 	
-	
 	//通过功能查询物料信息
 	@RequestMapping("admin_viewHardware.do")
 	public String selHardware(ModelMap map, String module) {
